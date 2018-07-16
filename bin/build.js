0088881ab9aa3f518c3ee67b7b58e271c57aba5f
Eirik Backer <Eirik.Backer@nrk.no>
2018-07-16 13:21:39 +0200

    Use new docs

--- bin/build.js
+++ bin/build.js
@@ -1,20 +1,23 @@
+const fs = require('fs');
 const path = require('path');
 const {version} = require('../package.json');
-const fs = require('fs');
+const lib = path.join(__dirname, '../lib');
 
-const CSS_FILENAME = 'core-css.css';
-const CSS_FILENAME_MIN = 'core-css.min.css';
-const LIB_PATH = path.join(__dirname, '../lib');
-
-const minifyCss = (css) => String(css)              // Work with file as string
-  .replace(/\/\*[^!][^*]*\*\//g, '')                // Strip comments
-  .replace(/\s*(^|[>:;,{}/]|$)\s*/g, '$1');         // Strip white space around tokens
+const minifyCss = (css) => String(css) // Work with file as string
+  .replace(/\/\*[^!][^*]*\*\//g, '') // Strip comments
+  .replace(/\s*(^|[>:;,{}/]|$)\s*/g, '$1') // Strip white space around tokens
 
 const buildCss = () => Promise
-  .resolve(fs.readFileSync(path.join(LIB_PATH, CSS_FILENAME)))
+  .resolve(fs.readFileSync(path.join(lib, 'core-css.css')))
   .then((css) => `/*! Core CSS v${version} - Copyright (c) 2015-${new Date().getFullYear()} NRK <opensource@nrk.no> */\n${css}`)
-  .then((css) => fs.writeFileSync(path.join(LIB_PATH, CSS_FILENAME_MIN), minifyCss(css)));
+  .then((css) => fs.writeFileSync(path.join(lib, 'core-css.min.css'), minifyCss(css)))
+
+const buildDocs = () => Promise
+  .resolve(fs.readFileSync(path.join(lib, 'docs.md')))
+  .then((md) => String(md).replace(/\/major\/\d+/, `/major/${version.match(/\d+/)}`))
+  .then((md) => fs.writeFileSync(path.join(lib, 'docs.md'), md))
 
 buildCss()
-  .then(() => console.log('Built CSS'))
-  .catch((err) => console.log(err.stack));
+  .then(() => buildDocs())
+  .then(() => console.log('Built CSS and docs'))
+  .catch((err) => console.log(err.stack))
