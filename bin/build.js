525be33efd730991bceb451fce167abb0879171c
Herman Torjussen <herman.torjussen@nrk.no>
2018-12-04 10:18:15 +0100

    clean up syntax

--- bin/build.js
+++ bin/build.js
@@ -5,7 +5,7 @@ const lib = path.join(__dirname, '../lib')
 const autoprefixer = require('autoprefixer')
 const postcss = require('postcss')
 
-const minifyCss = (css) => String(css) // Work with file as string
+const minifyCss = css => String(css) // Work with file as string
   .replace(/\/\*[^!][^*]*\*\//g, '') // Strip comments
   .replace(/\s*(^|[>:;,{}/]|$)\s*/g, '$1') // Strip white space around tokens
 
@@ -17,10 +17,10 @@ const buildCss = () => Promise
 
 const buildDocs = () => Promise
   .resolve(fs.readFileSync(path.join(lib, 'docs.md')))
-  .then((md) => String(md).replace(/\/major\/\d+/, `/major/${version.match(/\d+/)}`))
-  .then((md) => fs.writeFileSync(path.join(lib, 'docs.md'), md))
+  .then(md => String(md).replace(/\/major\/\d+/, `/major/${version.match(/\d+/)}`))
+  .then(md => fs.writeFileSync(path.join(lib, 'docs.md'), md))
 
 buildCss()
   .then(() => buildDocs())
   .then(() => console.log('Built CSS and docs'))
-  .catch((err) => console.log(err.stack))
+  .catch(err => console.log(err.stack))
