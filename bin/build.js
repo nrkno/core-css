66337c224c691ae229558c047991082eaa09cf24
Herman Torjussen <herman.torjussen@nrk.no>
2018-12-20 12:38:30 +0100

    output less, sass, styl; include in npm; ignore in git

--- bin/build.js
+++ bin/build.js
@@ -1,9 +1,10 @@
 const { JSDOM } = require('jsdom')
 const fs = require('fs')
 const path = require('path')
-const cssFile = path.join(__dirname, '../lib/core-css.css')
-const dom = new JSDOM(`<style>${fs.readFileSync(cssFile)}</style>`)
+const lib = path.join(__dirname, '../lib')
+const dom = new JSDOM(`<style>${fs.readFileSync(path.join(lib, 'core-css.css'))}</style>`)
 const css = dom.window.document.styleSheets[0]
+const extensions = ['sass', 'less', 'styl']
 
 // Returns array of [[selector, props], ...] to support multiple equal selectors and consistent order
 function parseCss ({ cssRules }) {
@@ -27,21 +28,35 @@ function parseStyle ({ style }) {
     [prop, `${style.getPropertyValue(prop)}${style.getPropertyPriority(prop).replace(/./, '!$&')}`])
 }
 
-function nestScss (rules) {
+function nestRules (rules, target) {
   const nested = []
   while (rules.length) {
     const [selector, rule] = rules.shift()
     const className = selector.match(/\.-?[_a-zA-Z]+[_a-zA-Z0-9-]*/)
 
     if (className) {
-      const mixin = `@mixin ${className[0].slice(1)}`
+      let mixin
+      switch (target) {
+        case 'sass': mixin = `@mixin ${className[0].slice(1)}`; break
+        case 'less': mixin = `${className[0]}()`; break
+        case 'styl': mixin = `${className[0].slice(1)}()`; break
+      }
       const parent = nested.filter(([m]) => m === mixin)[0] || nested[nested.push([mixin, []]) - 1]
       const prefix = selector.slice(0, className.index)
-      const suffix = selector.slice(className.index + className[0].length).replace(className[0], '#{&}')
+      const suffix = selector.slice(className.index + className[0].length)
 
-      if (prefix) parent[1].push([`@at-root ${prefix}#{&}${suffix}`, rule])
-      else if (suffix) parent[1].push([`&${suffix}`, rule])
-      else parent[1].push(...rule)
+      if (prefix) {
+        let tagSelector
+        switch (target) {
+          case 'sass': tagSelector = `@at-root ${prefix}#{&}${suffix.replace(className[0], '#{&}')}`; break
+          case 'less': case 'styl': tagSelector = `${prefix}&${suffix}`; break
+        }
+        parent[1].push([tagSelector, rule])
+      } else if (suffix) {
+        parent[1].push([`&${suffix}`, rule])
+      } else {
+        parent[1].push(...rule)
+      }
     } else {
       nested.push([selector, rule])
     }
@@ -56,13 +71,8 @@ function toString (rules, pad = '') {
   }).join('')
 }
 
-// nestScss(parseCss(css))
-console.log(toString(nestScss(parseCss(css))))
-
-fs.writeFileSync('test.scss',
-  toString(
-    nestScss(
-      parseCss(css)
-    )
-  )
-)
+extensions.forEach(ext => {
+  const result = toString(nestRules(parseCss(css), ext))
+  fs.writeFileSync(path.join(lib, `core-css.${ext}`), result)
+  fs.writeFileSync(path.join(__dirname, '..', `core-css.${ext}`), result)
+})
--- rollup.config.js
+++ rollup.config.js
@@ -12,23 +12,29 @@ if (!process.env.ROLLUP_WATCH) {
   fs.writeFileSync(path.join('lib', 'readme.md'), versioned)
 }
 
+const plugins = [
+  postcss({
+    minimize: {
+      reduceIdents: {
+        keyframes: false
+      }
+    },
+    sourceMap: !process.env.ROLLUP_WATCH,
+    plugins: [
+      autoprefixer({ browsers: ['last 1 version', '> .1%', 'ie 9-11'] }),
+      header({ header: `/*! @nrk/core-css v${version} - Copyright (c) 2018-${new Date().getFullYear()} NRK */` })
+    ],
+    extract: true
+  }),
+  !process.env.ROLLUP_WATCH || serve('lib')
+]
+
 export default [{
   input: 'lib/core-css.js',
   output: { file: 'lib/core-css.min.js', format: 'cjs' },
-  plugins: [
-    postcss({
-      minimize: {
-        reduceIdents: {
-          keyframes: false
-        }
-      },
-      sourceMap: !process.env.ROLLUP_WATCH,
-      plugins: [
-        autoprefixer({ browsers: ['last 1 version', '> .1%', 'ie 9-11'] }),
-        header({ header: `/*! @nrk/core-css v${version} - Copyright (c) 2018-${new Date().getFullYear()} NRK */` })
-      ],
-      extract: true
-    }),
-    !process.env.ROLLUP_WATCH || serve('lib')
-  ]
+  plugins
+}, {
+  input: 'lib/core-css.js',
+  output: { file: 'core-css.min.js', format: 'cjs' },
+  plugins
 }]
