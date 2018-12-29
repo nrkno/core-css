ec372540d81ba27723e8aab8a7197224d5928190
Eirik Backer <Eirik.Backer@nrk.no>
2018-12-29 18:01:41 +0100

    Simplify RollupConfig

--- rollup.config.js
+++ rollup.config.js
@@ -13,29 +13,27 @@ if (!process.env.ROLLUP_WATCH) {
   fs.writeFileSync(path.join('lib', 'readme.md'), versioned)
 }
 
-const plugins = [
-  postcss({
-    extract: true,
-    minimize: { reduceIdents: { keyframes: false } },
-    sourceMap: !process.env.ROLLUP_WATCH,
-    plugins: [
-      autoprefixer({ browsers: ['last 1 version', '> .1%', 'ie 9-11'] }),
-      header({ header: `/*! @nrk/core-css v${version} - Copyright (c) 2018-${new Date().getFullYear()} NRK */` }),
-      classesToMixins({
-        styl: './core-css.styl',
-        scss: './core-css.scss',
-        less: './core-css.less'
-      })
-    ]
-  }),
-  !process.env.ROLLUP_WATCH || serve('lib')
-]
-
 export default [{
   input: 'lib/core-css.js',
   output: [
     { format: 'cjs', file: 'core-css.min.js' },
     { format: 'cjs', file: 'lib/core-css.min.js' }
   ],
-  plugins
+  plugins: [
+    postcss({
+      extract: true,
+      minimize: { reduceIdents: { keyframes: false } },
+      sourceMap: !process.env.ROLLUP_WATCH,
+      plugins: [
+        autoprefixer({ browsers: ['last 1 version', '> .1%', 'ie 9-11'] }),
+        header({ header: `/*! @nrk/core-css v${version} - Copyright (c) 2018-${new Date().getFullYear()} NRK */` }),
+        classesToMixins({
+          styl: './core-css.styl',
+          scss: './core-css.scss',
+          less: './core-css.less'
+        })
+      ]
+    }),
+    !process.env.ROLLUP_WATCH || serve('lib')
+  ]
 }]
